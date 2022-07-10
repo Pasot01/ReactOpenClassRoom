4914b228d69b9482753b2c3188c68b6a94b4c118
Pasot01 <contactmyphone14@gmail.com>
2022-07-10 21:30:24 +0200

    Manipulation des states et amélioration du panier

--- workflow/la-maison-jungle/src/components/Banner.js
+++ workflow/la-maison-jungle/src/components/Banner.js
@@ -1,29 +1,7 @@
-import logo from '../assets/logo.png'
 import '../styles/Banner.css'
 
-function Banner() {
-  const title = "La maison jungle"
-  return (
-    <div className="lmj-banner">
-       <img src={logo} alt='La maison jungle' className='lmj-logo' />
-      <h1 className="lmj-title">{title}</h1>,
-    </div>
-  )
+function Banner({ children }) {
+	return <div className='lmj-banner'>{children}</div>
 }
-
-// Autre manière de styler son composant
-// function Banner() {
-//   const title = "La maison jungle"
-//   return (
-//     <div style={{
-//       color: 'black',
-//       textAlign: 'right',
-//       padding: 32,
-//       borderBottom: 'solid 3px black'
-//     }}>
-//       <h1>{title}</h1>,
-//     </div>
-//   )
-// }
 
 export default Banner
