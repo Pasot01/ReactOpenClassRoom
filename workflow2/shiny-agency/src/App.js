2a240b39791c75e536936fc9409c354c08a8af67
Pasot01 <contactmyphone14@gmail.com>
2022-07-20 20:29:14 +0200

    Restructuration des dossiers et de leurs composants, ainsi que le routage avec les nouvelles fonctionnalités

---
+++ workflow2/shiny-agency/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home/home'
+import Survey from './pages/Survey/survey'
+import Results from './pages/Results/result'
+import Freelances from './pages/Freelances/freelance'
+import Header from './components/Header/header'
+import Error from './components/Error/error'
+
+export default function App() {
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/survey/:questionNumber" element={<Survey />} />
+                <Route path="/results" element={<Results />} />
+                <Route path="/freelances" element={<Freelances />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
+        </>
+    );
+}
