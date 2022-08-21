763f7f0b226b6d914fa9979594a0f48ac2d277a1
Pasot01 <contactmyphone14@gmail.com>
2022-08-21 22:42:46 +0200

    Configuration de styled-components et de react-router-dom

--- workflow2/shiny-agency/src/App.js
+++ workflow2/shiny-agency/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home/home'
-import Survey from './pages/Survey/survey'
-import Results from './pages/Results/result'
-import Freelances from './pages/Freelances/freelance'
-import Header from './components/Header/header'
-import Error from './components/Error/error'
+import Home from './pages/Home/home';
+import Survey from './pages/Survey/survey';
+import Results from './pages/Results/result';
+import Freelances from './pages/Freelances/freelance';
+import Header from './components/Header/header';
+import Error from './components/Error/error';
 
 export default function App() {
     return (
--- workflow2/shiny-agency/src/index.js
+++ workflow2/shiny-agency/src/index.js
@@ -2,12 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+    body {
+      margin: 0;
+    }
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <GlobalStyle />
+        <App />
     </BrowserRouter>
   </React.StrictMode>
 );
